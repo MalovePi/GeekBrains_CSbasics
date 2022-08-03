b5abfcd0e6e68106d578055c288df4e012c227bd
MalovePi <p.malove@yandex.ru>
2022-08-03 19:49:33 +0300

    add lesson 02

---
+++ geekBrains_CSbasics/geekBrains_CSbasics_Lesson_02/Lesson_02.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geekBrains_CSbasics_Lesson_02
+{
+    class Lesson_02
+    {
+        static void Main()
+        {
+        }
+    }
+}
