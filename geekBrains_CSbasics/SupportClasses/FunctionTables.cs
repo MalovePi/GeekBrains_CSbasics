d58e7f2d9319318d837ffdd2b47b5e7fbc136e93
MalovePi <p.malove@yandex.ru>
2022-08-23 21:12:24 +0300

    Домашняя работа
    Решение первой задачи
    Использование делегатов

---
+++ geekBrains_CSbasics/SupportClasses/FunctionTables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportClasses
+{
+    public class FunctionTables
+    {
+        public delegate double Function(double a, double x);
+
+        public static double Squaring(double a, double x) => a * Math.Pow(x, 2);
+
+        public static double Sin(double a, double x) => a * Math.Sin(x);
+    }
+}
